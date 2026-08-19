f3cfd575bf1e646d232e114306ea3ee85001c5bc
LuisYepes601 <tucorreo@example.com>
2026-08-19 13:45:11 -0500

    first commit

---
+++ src/main/java/com/nexus/biblioNepo/BiblioNepoApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.biblioNepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioNepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioNepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus/biblioNepo/BiblioNepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.biblioNepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioNepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
