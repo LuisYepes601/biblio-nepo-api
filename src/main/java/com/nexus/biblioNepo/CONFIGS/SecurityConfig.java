578802ae70214f43e6813d80813c3bd211aa40d9
LuisYepes601 <tucorreo@example.com>
2026-09-08 15:23:47 -0500

    agregando security y jwt

--- src/main/java/com/nexus/biblioNepo/CONFIGS/SecurityConfig.java
+++ src/main/java/com/nexus/biblioNepo/CONFIGS/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,6 +36,7 @@ public class SecurityConfig {
 
         httpSecurity
                 // Desactivamos CSRF porque nuestra API utilizará JWT
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 // No utilizaremos sesiones
                 .sessionManagement(session
