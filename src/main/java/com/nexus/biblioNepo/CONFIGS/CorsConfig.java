e0805ac950da591df889c00aa06365b1b70e283b
LuisYepes601 <tucorreo@example.com>
2026-09-08 15:15:44 -0500

    agregando security y jwt

--- src/main/java/com/nexus/biblioNepo/CONFIGS/CorsConfig.java
+++ src/main/java/com/nexus/biblioNepo/CONFIGS/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
