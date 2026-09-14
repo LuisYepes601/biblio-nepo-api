32bf4d1929d83d90ef8086a054f49ef71c330611
LuisYepes601 <tucorreo@example.com>
2026-09-14 15:46:17 -0500

    configurando security

--- src/main/java/com/nexus/biblioNepo/CONTROLLERS/Home.java
+++ src/main/java/com/nexus/biblioNepo/CONTROLLERS/Home.java
@@ -2,10 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.nexus.biblioNepo.CONTROLLERS.Uptime;
+package com.nexus.biblioNepo.CONTROLLERS;
 
-import java.util.Map;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,18 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author luis
  */
-@RequestMapping(value = "/api/v1/life")
+@RequestMapping(value = "/")
 @RestController
-public class Uptime {
+public class Home {
 
-    @GetMapping()
-    public ResponseEntity<Map<String, String>> isLife() {
-
-        return ResponseEntity
-                .ok()
-                .body(Map.of("message", "is live"));
+    @GetMapping
+    public String inicio() {
+        return "BiblioNepo API funcionando correctamente 🚀";
     }
-
-  
 
 }
